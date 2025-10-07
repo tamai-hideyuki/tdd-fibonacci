a61ce8355b75ce13563d681eeb9242c02279c575
tamai-hideyuki <hide27yuki@gmail.com>
2025-10-08 07:22:38 +0900

    ✨ 初期設定〜一つ目のテストまで実装

---
+++ packages/fibonacci/tests/fibonacci.test.ts
@@ -0,0 +1,25 @@
+import { fibonacci } from '../src/fibonacci';
+
+describe (`fibonacci`, () => {
+    test(`最初のフィボナッチ数は1`, ()=> {
+        expect(fibonacci(1)).toBe(1);
+    });
+
+    // test(`２番目のフィボナッチ数は１`, ()=> {
+    //     expect(fibonacci(2)).toBe(1);
+    // });
+
+    // test(`3番目の数は2`, ()=> {
+    //     expect(fibonacci(3)).toBe(2);
+    // });
+
+    // test(`4番目の数は3`, ()=> {
+
+    // });
+
+    // test(`5番目の数は5`, ()=> {
+
+    // });
+    
+    
+});
